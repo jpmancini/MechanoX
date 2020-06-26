e9ffa44f581d298518aa39017bc3469c5d3ea685
jpmancini <jpmancini623@gmail.com>
2020-06-26 19:07:00 -0400

    created RegisterScreen.js

--- App.js
+++ App.js
@@ -3,64 +3,9 @@ import { StyleSheet, Button, Image, Text, TextInput, TouchableOpacity, View } fr
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import LoginScreen from './src/screens/LoginScreen.js'
-import MainScreen from './src/screens/MainScreen.js'
-
-function RegisterScreen({ navigation }) {
-  return (
-    <>
-      <View style={loginStyles.loginContainer}>
-        <Image
-          resizeMode = 'contain'
-          style={loginStyles.image}
-          source={require('./res/images/Logo.jpg')}
-        />
-        <TextInput 
-          style={loginStyles.fillIn}
-          textAlign={'center'}
-          placeholder='Full Name'
-        />
-        <TextInput
-          style={loginStyles.fillIn}
-          textAlign={'center'}
-          placeholder='Email Address'
-        />
-        <TextInput 
-          style={loginStyles.fillIn}
-          textAlign={'center'}
-          placeholder='Phone Number'
-        />
-        <TextInput
-          style={loginStyles.fillIn}
-          textAlign={'center'}
-          placeholder='Password'
-        />
-        <TextInput
-          style={loginStyles.fillIn}
-          textAlign={'center'}
-          placeholder='Confirm Password'
-        />
-        <View style={[{ width: "70%", margin: 10,}]}>
-          <TouchableOpacity
-            style={loginStyles.loginScreenButton}
-            onPress = {() => navigation.navigate('Main')}
-            underlayColor='#fff'
-          >
-            <Text style={loginStyles.loginButtonText}>Register</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-
-      <View style={loginStyles.footer}>
-        <TouchableOpacity
-          //onPress = {() => navigation.navigate('Login')}
-        >
-          <Text style={loginStyles.termsText}>By registering, you agree to all of our terms and conditions</Text>
-        </TouchableOpacity>    
-      </View>
-    </>
-  );
-}
+import LoginScreen from './src/screens/LoginScreen.js';
+import MainScreen from './src/screens/MainScreen.js';
+import RegisterScreen from './src/screens/RegisterScreen.js';
 
 function BookingsScreen( { navigation }) {
   return (
