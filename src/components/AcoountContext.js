b5c5b99686a8d998ff910a3f3f2b5a7917a8c05e
jpmancini <jpmancini623@gmail.com>
2020-07-06 15:45:59 -0400

    Update AcoountContext.js

--- src/components/AcoountContext.js
+++ src/components/AcoountContext.js
@@ -1,4 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, createContext} from 'react';
+import { useLinkProps } from '@react-navigation/native';
+
+export const AccountContext = createContext();
 
 const AccountProvider = () => {
     const [account, setAccount] = useState([
@@ -11,8 +14,10 @@ const AccountProvider = () => {
     ]);
 
     return(
-
+        <AccountContext.Provider>
+            {useLinkProps.children}
+        </AccountContext.Provider>
     );
 }
 
-export default Username;
+export default AccountProvider;
